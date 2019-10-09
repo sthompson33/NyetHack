47d0a3537882a37cbf500111d393b0adf7107c7f
Stephen Thompson <thompson.stephen33@gmail.com>
2019-10-09 17:57:09 -0400

    Intro to Maps added to Tavern.kt

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -3,16 +3,25 @@ import java.io.File
 
 const val TAVERN_NAME = "Taernyl's Folly"
 
-var playerGold = 10
-var playerSilver = 10
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
 val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
 val uniquePatrons = mutableSetOf<String>()
 //changed reading a file to readLines().
 //previous readText().split("\n) threw indexOutOfBoundsException because end of file line (blank) was included
 val menuList = File("data/tavern-menu-items.txt").readLines()
+val patronGold = mutableMapOf<String, Double>()
 
 fun main() {
+    if(patronList.contains("Eli")) {
+        println("The tavern master says: Eli's in the back playing cards.")
+    } else {
+        println("The tavern master says: Eli isn't here.")
+    }
+    if(patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    } else {
+        println("The tavern master says: Nay, they left hours ago.")
+    }
     (0..9).forEach() {
         val first = patronList.shuffled().first()
         val last = lastName.shuffled().first()
@@ -20,37 +29,28 @@ fun main() {
         uniquePatrons += name
         }
 
-    println(uniquePatrons)
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
+    }
 
     var orderCount = 0
     while (orderCount <= 9) {
         placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
         orderCount++
     }
+
+    displayPatronBalances()
 }
 
-private fun performPurchase(price: Double){
-    displayBalance()
-    val totalPurse = playerGold + (playerSilver / 100.0)
-
-    println("Total purse: $totalPurse")
-    println("Purchasing item for $price")
-
-    val remainingBalance = totalPurse - price
-    require(remainingBalance >= 0) { "Not enough funds" }
-    //when printing to the console, remainingBalance is formatted to two decimals 4.19
-    //however, remainingBalance still has the floating point value
-    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
-
-    val remainingGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
-    displayBalance()
+private fun displayPatronBalances(){
+    patronGold.forEach {patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
 }
 
-private fun displayBalance(){
-    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
+private  fun performPurchase(price: Double, patronName: String){
+    val totalPurse = patronGold.getValue(patronName)
+    patronGold[patronName] = totalPurse - price
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -74,7 +74,7 @@ private fun placeOrder(patronName: String, menuData: String){
     val message = "$patronName buys a $name ($type) for $price"
     println(message)
 
-    //performPurchase(price.toDouble())
+    performPurchase(price.toDouble(), patronName)
 
     val phrase = if (name == "Dragon's Breath") {
         "$patronName exclaims: ${toDragonSpeak("Ah, delicious $name")}"
